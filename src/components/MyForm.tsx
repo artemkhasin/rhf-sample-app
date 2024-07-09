c520c73ff32303b7db82a8f3333036cd4ed15cb9
artemkhasin <artem.khasin@aizon.ai>
2024-07-09 15:44:58 +0200

    stepper

--- src/components/MyForm.tsx
+++ src/components/MyForm.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useForm, SubmitHandler, FormProvider } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, Stack } from '@mui/material';
+import { Button, Stack, Step, StepLabel, Stepper } from '@mui/material';
 import { schema, FormData } from '../types/schema';
 import AdditionalInfo from './AdditionalInfo';
 import MainInfo from './MainInfo';
@@ -13,24 +13,55 @@ function MyForm() {
 
   const { handleSubmit } = methods;
 
+  const steps = ['Main Info', 'Additional Info'];
+  const [activeStep, setActiveStep] = React.useState(1);
 
   const onSubmit: SubmitHandler<FormData> = (data) => {
     console.table(data);
   };
 
   const handleClicked = () => {
-    handleSubmit(onSubmit)();
+    if (activeStep < steps.length) {
+      setActiveStep((prev) => prev + 1);
+    }
+    if (activeStep === steps.length - 1) {
+        return handleSubmit(onSubmit)();
+    }
   };
 
   return (
         <FormProvider {...methods}>
             <Stack spacing={3} width={300} margin='auto' marginTop={5} direction='column'>
-                <MainInfo />
-                <AdditionalInfo />
-                <Button type="submit" variant='contained' onClick={handleClicked}>Submit</Button>
+                <Stepper activeStep={activeStep} alternativeLabel>
+                    {steps.map((label) => (
+                        <Step key={label}>
+                            <StepLabel>{label}</StepLabel>
+                        </Step>
+                    ))}
+                </Stepper>
+                {activeStep === 1 && <MainInfo />}
+                {activeStep === 2 && <AdditionalInfo />}
+                <Stack spacing={2} direction='row' justifyContent='center'>
+                    {
+                        activeStep !== 1 && 
+                        <Button 
+                            variant='outlined' 
+                            onClick={()=> setActiveStep(prev => prev - 1)}
+                            fullWidth
+                        >
+                            Back
+                        </Button>}
+                    <Button 
+                        type="submit" 
+                        variant='contained' 
+                        onClick={handleClicked}
+                        fullWidth
+                    >
+                        {activeStep === steps.length ? 'Submit' : 'Next'}
+                    </Button>
+                </Stack>
             </Stack>
         </FormProvider>
-
   );
 }
 
